a9b865978702909e567f1e7be5063a0cd0c60884
Eva Szatmari <szateva@gmail.com>
2021-05-27 22:52:56 +0100

    Ex1 written all in main() and works, why?

--- src/Ex1_MailingAddress.java
+++ src/Ex1_MailingAddress.java
@@ -1,20 +1,18 @@
 /* display name and mailing address */
+import java.util.*;
+import java.io.*;
 
 public class Ex1_MailingAddress {
-    String name = "Joe Doe";
-    String address1 = "20 Main Street";
-    String address2 = "London";
-    String address3 = "WC1E 7HX ";
+    
+    public static void main(String[] args) {
+        String name = "Joe Doe";
+        String address1 = "20 Main Street";
+        String address2 = "London";
+        String address3 = "WC1E 7HX ";
 
-    public void printAddress(){
         System.out.println(name);
         System.out.println(address1);
         System.out.println(address2);
         System.out.println(address3);
-    }
-
-
-    public static void main(String[] args) {
-        printAddress();
     }
 }
