ee2ac119ca848b19313de8ba5e28fc5f2ec3c4ec
Eva Szatmari <szateva@gmail.com>
2021-05-27 22:39:00 +0100

    Initial commit, coding The Python Workbook exercises in Java.

---
+++ src/Ex1_MailingAddress.java
@@ -0,0 +1,19 @@
+/* display name and mailing address */
+
+public class Ex1_MailingAddress {
+    String name = "Joe Doe";
+    String address1 = "20 Main Street";
+    String address2 = "London";
+    String address3 = "WC1E 7HX ";
+    
+    public static String printAddress(){
+        System.out.println(name);
+        System.out.println(address1);
+        System.out.println(address2);
+        System.out.println(address3);
+    }
+    }
+
+    public static void main(String[] args) {
+        printAddress();
+    }
